0a6182214627bab304e7588c84ca83c890c59802
G0Gi-cw <gogolyakpeter@gmail.com>
2024-11-27 23:08:24 +0100

    asd
    asd

--- Assets/Scripts/Events/EventManager.cs
+++ Assets/Scripts/Events/EventManager.cs
@@ -2,6 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< Updated upstream
+=======
+using UnityEngine.Events;
+public static class EventManager
+{
+    public static event UnityAction StageEnd;
+    public static event UnityAction Pause;
+ 
+    public static void OnStageEnd() => StageEnd?.Invoke();
+    public static void OnPause() => Pause?.Invoke();
+>>>>>>> Stashed changes
 
 public class EventManager : MonoBehaviour
 {
