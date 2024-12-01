73348fd131b29f710680226e0500d227837f836c
molnarjakab1 <molnarjakab4@gmail.com>
2024-12-01 16:34:13 +0100

    bombaclat

--- Assets/Scripts/Events/EventManager.cs
+++ Assets/Scripts/Events/EventManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
+<<<<<<< Updated upstream
 
     public Dictionary<int, Enemy> enemyIDs;
     public int enemys = 0;
@@ -14,4 +15,17 @@ public class EventManager : MonoBehaviour
         EnemyDeathAfterEvent?.Invoke(id);
         
     }
+=======
+    public static event UnityAction StageEnd;
+    public static event UnityAction Pause;
+    public static event UnityAction CoinCollected;
+    public static event UnityAction Upgrade;
+    public static event UnityAction GameEnd;
+    public static void OnStageEnd() => StageEnd?.Invoke();
+    public static void OnPause() => Pause?.Invoke();
+    public static void OnCoinCollected() => CoinCollected?.Invoke();
+    public static void OnUpgrade() => Upgrade?.Invoke();
+
+    public static void OnGameEnd()=> GameEnd?.Invoke();
+>>>>>>> Stashed changes
 }
